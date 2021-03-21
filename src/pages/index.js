97b6ade3b3c8cd4ad82221f2edf87b2737c2cda0
AlexHilson <alexhilsonwork@googlemail.com>
2021-03-21 21:52:45 +0000

    intro blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <ContentBlock>
     <p>sensewithDATA brings together a collective of sensemakers, seeking to start a discussion about how digital data affects communities and becomes a force for change. How human data enables the creation and delivery of highly personal experiences, and where organisational data can enable rapid and potentially life-changing decisions. We intend this work to have an awareness and understanding of the human and social values that must underpin the development of new technologies that capture and analyse data. We like asking questions, provking discourse and exploring a diversity of attitudes and approaches, as a method of research and developing our data practice.</p>
 
-    <p>There are two key parts to this work. In <a href="/questions/">'questions'</a>, we host a virtual panel discussion about the burning questions facing data sensemakers. And in <a href="/resources/">'resources'</a>, we've put together a set of links to interesting articles, exhibitions, and papers.</p>
+    <p>There are a few key parts to this work. In <a href="/questions/">'questions'</a>, we host a virtual panel discussion about the burning questions facing data sensemakers. In <a href="/resources/">'resources'</a>, we've put together a set of links to interesting articles, exhibitions, and papers. And in <a href="/voices/">'voices'</a> we widen the conversation and ask others to introduce their own data voices and burning questions.</p>
     </ContentBlock>
   </Container>
   )
